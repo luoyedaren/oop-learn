2e414f085614e6e7363f1f2f6ba358a16f3a6059
chenghai <287623909@qq.com>
2019-01-23 16:59:12 +0800

    修改错误的单例写法

--- src/main/java/com/github/luoyedaren/learnoop/singleton/ConnectionDemo2.java
+++ src/main/java/com/github/luoyedaren/learnoop/singleton/ConnectionDemo2.java
@@ -1,11 +1,13 @@
 package com.github.luoyedaren.learnoop.singleton;
 
+import org.springframework.util.Assert;
+
 /**
  * project learn-oop
  *
  * @author chenghai on 2019/1/21 0021. - 星期一
  * nickName louyedaren
- *
+ * 懒加载
  * 单线程
  */
 public class ConnectionDemo2 {
@@ -15,10 +17,19 @@ public class ConnectionDemo2 {
 	private ConnectionDemo2() {
 	}
 
-	public ConnectionDemo2 getInstance(){
+	public static ConnectionDemo2 getInstance() {
 		if (connectionDemo == null) {
-			return new ConnectionDemo2();
+			connectionDemo = new ConnectionDemo2();
 		}
 		return connectionDemo;
+	}
+
+	public static void main(String[] args) {
+		ConnectionDemo2 instance1 = ConnectionDemo2.getInstance();
+		ConnectionDemo2 instance2 = ConnectionDemo2.getInstance();
+		System.out.println(instance1);
+		System.out.println(instance2);
+		Assert.notNull(instance1, "空的");
+		Assert.isTrue(instance1.equals(instance2), "相同");
 	}
 }
